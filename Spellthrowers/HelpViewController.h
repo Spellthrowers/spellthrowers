7551b772357564a8984bb3a05a43712f37d5b7e8
mpieler <mipie22@gmail.com>
2014-10-18 14:30:31 -0400

    starting to add help menu

---
+++ Spellthrowers/HelpViewController.h
@@ -0,0 +1,14 @@
+//
+//  HelpViewController.h
+//  Spellthrowers
+//
+//  Created by Michelle on 18.10.14.
+//  Copyright (c) 2014 Spellthrowers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <AudioToolbox/AudioToolbox.h>
+
+@interface HelpViewController : UIViewController
+
+@end
---
+++ Spellthrowers/HelpViewController.m
@@ -0,0 +1,32 @@
+//
+//  HelpViewController.m
+//  Spellthrowers
+//
+//  Created by Michelle on 18.10.14.
+//  Copyright (c) 2014 Spellthrowers. All rights reserved.
+//
+
+#import "HelpViewController.h"
+
+
+@interface HelpViewController ()
+
+@end
+
+@implementation HelpViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+
+@end
